cde8999ceecb5fdcc9eeedfbeb39c8a4db8e7c34
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2020-10-02 19:45:51 -0300

    Pratica aula-26. dica importante utilização do nextLine() para consumir linhas pendentes

---
+++ src/aula26.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class aula26 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// Lendo texto e + dica pulo do gato para utilizar nextLine()
+		int x = sc.nextInt();
+		sc.nextLine();
+		String s1 = sc.nextLine();
+		String s2 = sc.nextLine();
+		String s3 = sc.nextLine();
+		
+		System.out.println("Você digitou: ");
+		System.out.println(x);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+		
+		
+		
+		sc.close();
+
+	}
+
+}
